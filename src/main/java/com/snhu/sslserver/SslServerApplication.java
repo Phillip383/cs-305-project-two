8bb3989218169098814ca08373dfccd08c3e0389
Phillip Wood <phillipwood2023@outlook.com>
2026-04-12 15:49:03 -0400

    Updated spring to current LTS. Added hash REST endpoint.

--- src/main/java/com/snhu/sslserver/SslServerApplication.java
+++ src/main/java/com/snhu/sslserver/SslServerApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SslServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SslServerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SslServerApplication.class, args);
+  }
 
 }
-//FIXME: Add route to enable check sum return of static data example:  String data = "Hello World Check Sum!";
+// FIXME: Add route to enable check sum return of static data example:  String data = "Hello World Check Sum!";
