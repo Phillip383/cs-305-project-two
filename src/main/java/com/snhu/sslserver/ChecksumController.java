402c822df0040285eb93155c14be67efd7055986
Phillip Wood <phillipwood2023@outlook.com>
2026-04-13 06:32:52 -0400

    Consuming post request from frontend.

--- src/main/java/com/snhu/sslserver/ChecksumController.java
+++ src/main/java/com/snhu/sslserver/ChecksumController.java
@@ -3,38 +3,41 @@ package com.snhu.sslserver;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.util.Map;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChecksumController {
 
-  @GetMapping("/hash")
-  public String hashData(String data) {
-      data = "Phillip Wood";
-    	String hexHash = "";
-    	
-    	try {
-    		MessageDigest digest = MessageDigest.getInstance("SHA-256");
-    		byte[] byteHash = digest.digest(data.getBytes(StandardCharsets.UTF_8));
-    		hexHash = byteToHex(byteHash);
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    	}
-    	
-        return "<p>data:"+data + "</p>" + "<p>Chiper Used: SHA-256" + " Checksum Value: " + hexHash + "</p>";
+  @PostMapping("/hash")
+  public String hashData(@RequestBody Map<String, String> payload) {
+    String hexHash = "";
+    String data = payload.get("data");
+
+    try {
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+      byte[] byteHash = digest.digest(data.getBytes(StandardCharsets.UTF_8));
+      hexHash = byteToHex(byteHash);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return hexHash;
   }
 
-    //Source: https://www.geeksforgeeks.org/java/sha-256-hash-in-java/
-    private static String byteToHex(byte[] bytes) {
-    	BigInteger n = new BigInteger(1, bytes);
-    	StringBuilder result = new StringBuilder(n.toString(16));
-    	
-    	while(result.length() < 64) {
-    		result.insert(0, '0');
-    	}
-    	
-    	return result.toString();
+  // Source: https://www.geeksforgeeks.org/java/sha-256-hash-in-java/
+  private static String byteToHex(byte[] bytes) {
+    BigInteger n = new BigInteger(1, bytes);
+    StringBuilder result = new StringBuilder(n.toString(16));
+
+    while (result.length() < 64) {
+      result.insert(0, '0');
     }
+
+    return result.toString();
+  }
 }
