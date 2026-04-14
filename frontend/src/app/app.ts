65daf3b990469cc4fe105dbd8c1ae5d97da7d5f8
Phillip Wood <phillipwood2023@outlook.com>
2026-04-14 05:11:27 -0400

    Hash value is reactive to data entry box. When the data in the entry box changes--the hash value box is hidden, and displayed when the form is sumbited again.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -14,11 +14,17 @@ export class App {
   protected readonly title = signal('Project Two');
   protected data = signal('');
   protected hash = signal('');
+  protected showHash = false;
+
+  protected onDataChanged(event: Event) {
+    this.showHash = false;
+  }
 
   async onSubmit(event: Event) {
     try {
       const response = await lastValueFrom(this.hashService.hash_request(this.data()));
       this.hash.set(response.toString());
+      this.showHash = true;
     } catch (error) {
       alert('Request Failed: ' + error);
     }
